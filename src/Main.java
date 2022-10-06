f29c2d8700c7919a320ada8d87bed26e7bb51745
Lenovo <altynbekuulubajsal@gmail.com>
2022-10-06 09:11:54 +0300

    added cat

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello java 7!");
         System.out.println("Dog");
-
+        System.out.println("Cat");
     }
 }
